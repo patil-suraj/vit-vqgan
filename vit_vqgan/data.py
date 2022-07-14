d2aa0f718580bb87d86d998c21baff1f70cab4c2
Boris Dayma <boris.dayma@gmail.com>
2022-07-14 03:05:33 +0000

    feat: filter dataset based on image size and ratio

--- vit_vqgan/data.py
+++ vit_vqgan/data.py
@@ -13,6 +13,8 @@ class Dataset:
     valid_folder: str = None
     batch_size: int = 64
     image_size: int = 256
+    min_image_size = 512
+    max_aspect_ratio = 2.0
     seed_dataset: int = None
     format: str = "rgb"  # rgb or lab
     train: tf.data.Dataset = field(init=False)
@@ -33,17 +35,44 @@ class Dataset:
         self.multi_hosts = jax.process_count() > 1
 
         # define parsing function
-        features = {"webp": tf.io.FixedLenFeature([], tf.string)}
+        features = {
+            "webp": tf.io.FixedLenFeature([], tf.string),
+            "original_width": tf.io.FixedLenFeature([], tf.int64),
+            "original_height": tf.io.FixedLenFeature([], tf.int64),
+        }
 
         def _parse_function(example_proto):
             parsed_features = tf.io.parse_single_example(example_proto, features)
-            return tfio.image.decode_webp(parsed_features["webp"])[..., :3]
+            return (
+                parsed_features["webp"],
+                parsed_features["original_width"],
+                parsed_features["original_height"],
+            )
+
+        def _filter_function(image, width, height):
+            # filter out images that are too small
+            if self.min_image_size is not None and (
+                width < self.min_image_size or height < self.min_image_size
+            ):
+                return False
+            # filter out images that have wrong aspect ratio
+            if self.max_aspect_ratio is not None and (
+                tf.divide(tf.maximum(width, height), tf.minimum(width, height))
+                > self.max_aspect_ratio
+            ):
+                return False
+            return True
+
+        def _parse_image(image, width, height):
+            return tfio.image.decode_webp(image)[..., :3]
 
         def _augment(image, seed):
             # create a new seed
             new_seed = tf.random.experimental.stateless_split(seed, num=1)[0, :]
             # apply random crop
-            return tf.image.stateless_random_crop(image, size=[self.image_size, self.image_size, 3], seed=new_seed)
+            return tf.image.stateless_random_crop(
+                image, size=[self.image_size, self.image_size, 3], seed=new_seed
+            )
 
         # augmentation wrapper
         def _augment_wrapper(image):
@@ -60,7 +89,9 @@ class Dataset:
                 # convert to lab
                 image = tfio.experimental.color.rgb_to_lab(image)
                 # project to [-1, 1]
-                return image / tf.constant([50.0, 128.0, 128.0]) + tf.constant([-1.0, 0.0, 0.0])
+                return image / tf.constant([50.0, 128.0, 128.0]) + tf.constant(
+                    [-1.0, 0.0, 0.0]
+                )
 
         for folder, dataset, augment in zip(
             [self.train_folder, self.valid_folder],
@@ -80,7 +111,17 @@ class Dataset:
                     ds = ds.repeat()
 
                 # parse dataset
-                ds = ds.map(_parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+                ds = ds.map(
+                    _parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE
+                )
+
+                # filter dataset
+                ds = ds.filter(_filter_function)
+
+                # parse image
+                ds = ds.map(
+                    _parse_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
+                )
 
                 if augment:
                     ds = ds.shuffle(1000)
@@ -92,6 +133,8 @@ class Dataset:
 
                 # batch, normalize and prefetch
                 ds = ds.batch(self.batch_size, drop_remainder=True)
-                ds = ds.map(_normalize, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+                ds = ds.map(
+                    _normalize, num_parallel_calls=tf.data.experimental.AUTOTUNE
+                )
                 ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
                 setattr(self, dataset, ds)
